c04649ef80f25dbd7d5b3ab7111b794f49effb41
Kat <kat.m.deans@gmail.com>
2025-01-06 10:49:15 -0500

    To do app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className="">My Todo List</h1>
+      <div>
+        <h2>Finish FES</h2>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
