aa9bebcd00834159b27b7c28d6ae24b57e2a004f
Kat <kat.m.deans@gmail.com>
2025-01-08 17:39:16 -0500

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,21 @@
 import './App.css';
-import Todo from './components/Todo'
-import Title from './components/Title'
-import Modal from './components/Modal.jsx';
-import Counter from './components/Counter.jsx'
-import React, { useState } from 'react';
+import{BrowserRouter, Routes, Route } from 'react-router'
+import Home from './pages/Home.jsx'
 
 
 function App() {
-  const [showModal, setShowModal] = useState(false)
 
-  function onTodoDelete() {
-    setShowModal(true)
-    console.log('onTodoDelete()')
-  }
-
-  function cancelModal() {
-    setShowModal(false)
-  }
-
-  function confirmModal() {
-    setShowModal(false)
-  }
 
 
   return (
     <div>
-      <Title></Title>
-        <div className='todo__wrapper'>
-          <Todo onTodoDelete={onTodoDelete} title="Finish FES"/>
-          <Todo onTodoDelete={onTodoDelete} title="Finish Interview Section"/>
-          <Todo onTodoDelete={onTodoDelete} title="Land $100k Job"/>
-        </div>     
-        {showModal && <Modal cancelModal={cancelModal} confirmModal={confirmModal} title="Confirm delete?"/>}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}>
+
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
