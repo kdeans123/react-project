ead94ba04a8f60d19443e6baa58c7688d455c7ad
Kat <kat.m.deans@gmail.com>
2025-01-08 19:55:37 -0500

    API integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css';
 import{BrowserRouter, Routes, Route } from 'react-router'
-import Home from './pages/Home.jsx'
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import Contact from '.pages/Contact.jsx';
 
 
 function App() {
@@ -11,9 +13,9 @@ function App() {
     <div>
       <Router>
         <Routes>
-          <Route path="/" element={<Home/>}>
-
-          </Route>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/about" element={<About/>}></Route>
+          <Route path="/contact" element={<Contact/>}></Route>
         </Routes>
       </Router>
     </div>
